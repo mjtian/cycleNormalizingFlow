415c48ab8fddef7dd1c2ba96243482655b60b8b3
mjtian <tianmeijing2009@163.com>
2019-04-24 00:27:55 +0800

    update

--- NICE.py
+++ NICE.py
@@ -8,35 +8,26 @@ class NICE(nn.Module): # fill in the parent class
         self.name = name
         self.tList = tList # init your inner layer list here, remember torch has it's own init method
 
-    def inverse(self,y):
+    def inverse(self,z):
         for i in range(len(self.tList)): # write the transmission of variables here, may take multiply lines.
-            net = SimpleMLP(i + 1, 50, 25, 8)
-            y = net.forward(y)
-        return y
+        x = z[:,:z.shape[1]/2]
+        y = z[:,z.shape[1]/2:]
+        y = y - u(x)
+        x = x - v(y)
+        z = torch.cat((x, y),1)
+        return z
 
     def forward(self,z):
         for i in range(len(self.tList)):  # write the transmission of variables here, may take multiply lines.
-            # net = SimpleMLP(i+1, 25, 50, 100)
-            # z = net.forward(z)
 
-        self.tList=[v,u]
+        x = z[:,:z.shape[1]/2]
+        y = z[:,z.shape[1]/2:]
+        x = x + v(y)
+        y = y + u(x)
+        z = torch.cat((x, y),1)
 
-        v_y = self.tList[0](y)
-        u_x = self.tList[1](x)
-
-        f0 =(x+v_y,y)
-        f1 =(x, y + u_x)
-
-        f = f0 * f1
         return z
 
 
 
-if __name__=='__main__':
-
-    net = NICE([100,8])
-
-    result1 = net.inverse
-    result2 = net.forward
-    print (result1.shape)
 
