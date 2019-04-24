2d0c7b3066e23a49cdd05ac7bb2f733f53948417
Li SH <li012589li@outlook.com>
2019-04-24 16:23:39 +0800

    udpate

---
+++ realnvp.py
@@ -0,0 +1,46 @@
+import torch
+from torch import nn
+from utils import SimpleMLP
+
+class Realnvp(nn.Module): # fill in the parent class
+    def __init__(self,sList,tList,name="NICE"):
+        super(Realnvp,self).__init__()
+        self.name = name
+        self.tList =nn.ModuleList(tList) # init your inner layer list here, remember torch has it's own init method
+
+    def inverse(self,z):
+        x = z[:,:z.shape[1]//2]
+        y = z[:,z.shape[1]//2:]
+        for i in range(len(self.tList)-1,-1,-1): # write the transmission of variables here, may take multiply lines.
+            if (i %2) ==0:
+                f = self.tList[i]
+                x = x - f(y )
+
+
+            else:
+
+                f = self.tList[i]
+                y = y - f(x)
+
+        z = torch.cat((x, y),1)
+        return z
+
+    def forward(self, z):
+        x = z[:,:z.shape[1]//2]
+        y = z[:,z.shape[1]//2:]
+        for i in range(len(self.tList)):  # write the transmission of variables here, may take multiply lines.
+            if (i %2) ==0:
+                f = self.tList[i]
+                x = x + f(y)
+            else:
+                f = self.tList[i]
+                y = y + f(x)
+
+
+        z = torch.cat((x, y),1)
+
+        return z
+
+
+
+
