75a6c2afc899fd5da14787e73e091bf81f2a4078
mjtian <tianmeijing2009@163.com>
2019-04-24 15:46:48 +0800

    change inverse

--- NICE.py
+++ NICE.py
@@ -11,13 +11,16 @@ class NICE(nn.Module): # fill in the parent class
     def inverse(self,z):
         x = z[:,:z.shape[1]//2]
         y = z[:,z.shape[1]//2:]
-        for i in range(len(self.tList)): # write the transmission of variables here, may take multiply lines.
+        for i in range(len(self.tList)-1,-1,-1): # write the transmission of variables here, may take multiply lines.
             if (i %2) ==0:
-                f = self.tList[-i]
-                y = y - f(x)
+                f = self.tList[i]
+                x = x - f(y )
+
+
             else:
-                f = self.tList[-i]
-                x = x - f(y)
+
+                f = self.tList[i]
+                y = y - f(x)
 
         z = torch.cat((x, y),1)
         return z
