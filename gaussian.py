949d24ab789c963d044209e81ce72e1c02da3231
mjtian <tianmeijing2009@163.com>
2019-04-25 19:04:20 +0800

    new

--- gaussian.py
+++ gaussian.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 class Gaussian(nn.Module):
     def __init__(self,shapeList,name="Gaussian"):
-      super(Gaussian,self).__init__()
+        super(Gaussian,self).__init__()
         self.name = name
         self.shapeList = shapeList
 
@@ -16,6 +16,9 @@ class Gaussian(nn.Module):
 
     def logProbability(self, z):
         return(-0.5*z**2 - 0.5*torch.log(2.*np.pi)).reshape(z.shape[0],-1).sum(dim=1)
+
+
+
 
 
 
