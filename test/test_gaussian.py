89059ba5e86018a962ce80c3641a5fdca8ff69cb
Li SH <li012589li@outlook.com>
2019-04-25 16:39:31 +0800

    add test

---
+++ test/test_gaussian.py
@@ -0,0 +1,16 @@
+import torch
+from gaussian import Gaussian
+
+def test_gaussian():
+    p = Gaussian([3,2,2])
+    x = p.sample(10)
+    assert x.shape[0] ==10
+    assert x.shape[1] ==3
+    assert x.shape[2] ==2
+    assert x.shape[3] ==2
+
+    t = torch.tensor([i for i in range(3*2*2)]).to(torch.float32).reshape(1,3,2,2)
+
+    logp = p.logProbability(t)
+
+    assert logp.item() == -264.0273
