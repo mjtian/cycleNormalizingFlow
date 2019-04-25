74649c99f69b81530616be998ef4bc5f486a87be
mjtian <tianmeijing2009@163.com>
2019-04-25 16:44:08 +0800

    sample

--- gaussian.py
+++ gaussian.py
@@ -1,13 +1,23 @@
 import torch
+import math
+import numpy as np
+import torch.nn as nn
 
 class Gaussian(nn.Module):
     def __init__(self,shapeList,name="Gaussian"):
       super(Gaussian,self).__init__()
         self.name = name
-        self.shapeList = nn.ModuleList(shapeList)
-    def sample(self,batch_s):
-        z = torch.randn(batch_s,self.shapeList)
+        self.shapeList = shapeList
+
+
+    def sample(self, batch_s):
+        size = [batch_s] +self.shapeList
+        z = torch.randn(size)
         return z
 
-    def logProbability(self,shape):
+    def logProbability(self, z):
+        return(-0.5*z**2 - 0.5*torch.log(2.*np.pi)).reshape(z.shape[0],-1).sum(dim=1)
+
+
+
 
