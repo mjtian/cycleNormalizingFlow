a8a66efeabd1e3781d49b4077d5947c9be9fb203
mjtian <tianmeijing2009@163.com>
2019-04-26 00:25:19 +0800

    complete logac

--- NICE.py
+++ NICE.py
@@ -9,20 +9,21 @@ class NICE(nn.Module): # fill in the parent class
         self.tList =nn.ModuleList(tList) # init your inner layer list here, remember torch has it's own init method
         self.prior =prior #<------note here!!
     def inverse(self,z):
-        inverseLogjac = z.new_zeros(z.shape[0]) <------note here!!
+        inverseLogjac = z.new_zeros(z.shape[0]) #<------note here!!
         x = z[:,:z.shape[1]//2]
         y = z[:,z.shape[1]//2:]
         for i in range(len(self.tList)-1,-1,-1): # write the transmission of variables here, may take multiply lines.
             if (i %2) ==0:
                 f = self.tList[i]
                 x = x - f(y )
+                inverseLogjac +=0
             else:
-
                 f = self.tList[i]
                 y = y - f(x)
+                inverseLogjac +=0
 
         z = torch.cat((x, y),1)
-        return z,inverseLogjac <------note here!!
+        return z,inverseLogjac #<------note here!!
 
     def forward(self, z):
         forwardLogjac = z.new_zeros(z.shape[0]) #<------note here!!
@@ -32,14 +33,15 @@ class NICE(nn.Module): # fill in the parent class
             if (i %2) ==0:
                 f = self.tList[i]
                 x = x + f(y)
+                forwardLogjac -=0
             else:
                 f = self.tList[i]
                 y = y + f(x)
-
+                forwardLogjac -=0
 
         z = torch.cat((x, y),1)
 
-        return z,forwardLogjac <------note here!!
+        return z,forwardLogjac #<------note here!!
 
     def sample(self,batchSize):
         pass
