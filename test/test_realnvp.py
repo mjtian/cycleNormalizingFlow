d12d71c1d76c07f03b751ecaf727cc0ea9be937f
mjtian <tianmeijing2009@163.com>
2019-04-26 16:21:43 +0800

    update

--- test/test_realnvp.py
+++ test/test_realnvp.py
@@ -13,24 +13,24 @@ def test_bijective():
     tList =[utils.SimpleMLP([4, 10, 4]), utils.SimpleMLP([4, 10, 4]),utils.SimpleMLP([4, 10, 4]), utils.SimpleMLP([4, 10, 4])]
     sList =[utils.SimpleMLP([4, 10, 4]), utils.SimpleMLP([4, 10, 4]),utils.SimpleMLP([4, 10, 4]), utils.SimpleMLP([4, 10, 4])]
     #x = torch.randn(1,8)
-     # Build your NICE net here, may take multiply lines.
+    #  # Build your NICE net here, may take multiply lines.
     # import pdb
     # pdb.set_trace()
     f = Realnvp(sList,tList,prior=p)
-    x = f.sample(100)
+    x = f.sample(10)
     op = f.logProbability(x)
     y,pi = f.inverse(x)
     pp = f.prior.logProbability(y)
     yx,pf = f.forward(y)
     yxy,pfi = f.inverse(yx)
 
-    assert_array_almost_equal(x.detach().numpy(),yx.detach().numpy())
-    assert_array_almost_equal(y.detach().numpy(),yxy.detach().numpy())
+    assert_array_almost_equal(x.detach().numpy(),yx.detach().numpy(), decimal=6)
+    assert_array_almost_equal(y.detach().numpy(),yxy.detach().numpy(), decimal=6)
 
-    assert_array_almost_equal(pi.detach().numpy(),-pf.detach().numpy())
-    assert_array_almost_equal(pf.detach().numpy(),-pfi.detach().numpy())
+    assert_array_almost_equal(pi.detach().numpy(),-pf.detach().numpy(), decimal=6)
+    assert_array_almost_equal(pf.detach().numpy(),-pfi.detach().numpy(), decimal=6)
 
-    assert_array_almost_equal((op+pi).detach().numpy(),pp.detach().numpy())
+    assert_array_almost_equal((op+pi).detach().numpy(),pp.detach().numpy(), decimal=6)
     assert_array_almost_equal((pp+pfi).detach().numpy(),op.detach().numpy())
 
 if __name__ == "__main__":
