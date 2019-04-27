c5a02f75f7f0588f4e541a2bc10c51e2a0f2778d
mjtian <tianmeijing2009@163.com>
2019-04-27 23:09:41 +0800

    update

--- main.py
+++ main.py
@@ -1,51 +1,51 @@
 from __future__ import division
 import numpy as np
 
-from utils import load_MNIST, random_draw #记得init
+from utils import load_MNIST, random_draw
 from realnvp import Realnvp
+import math
 
 def train():
-    np.random.seed(5)
-    batch_size = 100
-    learning_rate = 0.5
-    num_epoch = 10
-    num_iteration = len(train_data) // batch_size
+    lr = 0.5
+    Epoch = 10
+    Iteration = len(train_data) // batch_size
+    Batchsize_test = 10
+    Batchsize_train = 100
     # an epoch means running through the training set roughly once
 
+    params = list(Realnvp.parameters())
+    params = list(filter(lambda p: p.requires_grad, params))
+    nparams = sum([np.prod(p.size()) for p in params])
+    print ('total nubmer of trainable parameters:', nparams)
+    optimizer = torch.optim.Adam(params, lr=lr, weight_decay=weight_decay)
+
     train_data, train_label, test_data, test_label = load_MNIST()
-    x, label = random_draw(test_data, test_ label, 1000)
+    x_test = random_draw(test_data, Batchsize_test)
     tList =[utils.SimpleMLP([4, 10, 4]), utils.SimpleMLP([4, 10, 4]),utils.SimpleMLP([4, 10, 4]), utils.SimpleMLP([4, 10, 4])]
     sList =[utils.SimpleMLP([4, 10, 4]), utils.SimpleMLP([4, 10, 4]),utils.SimpleMLP([4, 10, 4]), utils.SimpleMLP([4, 10, 4])]
-    p = Gaussian([8])
+    p = Gaussian([28,28])
     f = Realnvp(sList,tList,prior=p)
-    result_= f.forward(x)
-    result = result_[0]
-    lgp =f.logProbability(x)
-    loss = -lgp.sum/batchsize
-    print('Before Training.\nTest loss = %.4f, correct rate = %.3f' % (loss, match_ratio(result, label)))
+    logp1 = f.logProbability(x_test)
+    loss1= - sum(logp1)/Batchsize_test
+    print('Before Training.\nTest loss = %.4f' %loss1)
 
-   for epoch in range(num_epoch):
-        for j in range(num_iteration):
-            x, label = random_draw(train_data, train_label, batch_size)
-            result_ = f.forward( x)
-            result = result_[0]
+    for epoch in range(Epoch):
+        for j in range(Iteration):
+           x_trian= random_draw(train_data,Batchsize_train)
+           logp = f.logProbability(x_trian)
+           loss = - sum(logp)/Batchsize_train
 
-            f_backward(f)
+           f.zero_grad()
+           loss.backward()
+           optimizer.step()
 
-            # update network parameters
-            # for node in net:
-            #     for p, p_delta in zip(node.parameters, node.parameters_deltas):
-            #         p -= learning_rate * p_delta  # stochastic gradient descent
+        print("epoch = %d/%d, loss = %.4f" %(epoch, loss))
 
-        print("epoch = %d/%d, loss = %.4f, corret rate = %.2f" %
-              (epoch, num_epoch, loss, match_ratio(result, label)))
+    x = random_draw(test_data, Batchsize_test)
+    logp2 = f.logProbability(x_test)
+    loss2= - sum(logp2)/Batchsize_test
+    print('After Training.\nTest loss = %.4f' %loss2)
 
-    x, label = random_draw(test_data, test_label, 1000)
-    result1= f_forward(x)
-    result2 = result1[0]
-    lgp =f.logProbability(x)
-    loss = -lgp.sum/batchsize
-    print('After Training.\nTest loss = %.4f, correct rate = %.3f' % (loss, match_ratio(result2, label)))
 
 
 if __name__ == "__main__":
