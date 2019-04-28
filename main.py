2703ec0e21a6504c06ae59ce48901a28458ecb2d
mjtian <tianmeijing2009@163.com>
2019-04-28 23:48:01 +0800

    update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from __future__ import division
 import numpy as np
+import torch
 from torchvision import transforms
 
 from utils import load_MNIST, random_draw ,SimpleMLP
@@ -14,33 +15,37 @@ def train():
     Batchsize_test = 10
     Batchsize_train = 100
     Iteration = len(train_data) // Batchsize_train
-
     # an epoch means running through the training set roughly once
-    transform = transforms.ToTensor()
+
 
 
     x_test = random_draw(test_data, Batchsize_test)
-    x_test1 = transform(x_test)
-    tList =[SimpleMLP([4, 10, 4]), SimpleMLP([4, 10, 4]),SimpleMLP([4, 10, 4]), SimpleMLP([4, 10, 4])]
-    sList =[SimpleMLP([4, 10, 4]), SimpleMLP([4, 10, 4]),SimpleMLP([4, 10, 4]), SimpleMLP([4, 10, 4])]
-    p = Gaussian([28,28])
+    x_test1 = torch.from_numpy(x_test)
+
+    x_test11 = x_test1.reshape(-1,28*28)
+    tList =[SimpleMLP([392, 196, 392]), SimpleMLP([392, 196, 392]),SimpleMLP([392, 196, 392]), SimpleMLP([392, 196, 392])]
+    sList =[SimpleMLP([392, 196, 392]), SimpleMLP([392, 196, 392]),SimpleMLP([392, 196, 392]), SimpleMLP([392, 196, 392])]
+    p = Gaussian([28*28])
     f = Realnvp(sList,tList,prior=p)
-    logp1 = f.logProbability(x_test1)
-    loss1= - sum(logp1)/Batchsize_test
+    import pdb
+    pdb.set_trace()
+    logp1 = f.logProbability(x_test11)
+    loss1= - logp1.mean
     print('Before Training.\nTest loss = %.4f' %loss1)
 
     params = list(Realnvp.parameters())
     params = list(filter(lambda p: p.requires_grad, params))
     nparams = sum([np.prod(p.size()) for p in params])
     print ('total nubmer of trainable parameters:', nparams)
-    optimizer = torch.optim.Adam(params, lr=lr, weight_decay=weight_decay)
+    optimizer = torch.optim.Adam(params, lr=lr)
 
     for epoch in range(Epoch):
         for j in range(Iteration):
            x_train= random_draw(train_data,Batchsize_train)
-           x_train1 = transform(x_train)
-           logp = f.logProbability(x_train1)
-           loss = - sum(logp)/Batchsize_train
+           x_train1 = torch.from_numpy(x_train)
+           x_train11 = x_train1.reshape(-1,28*28)
+           logp = f.logProbability(x_train11)
+           loss = -logp.mean
 
            f.zero_grad()
            loss.backward()
@@ -49,8 +54,10 @@ def train():
         print("epoch = %d/%d, loss = %.4f" %(epoch, loss))
 
     x = random_draw(test_data, Batchsize_test)
-    logp2 = f.logProbability(x_test)
-    loss2= - sum(logp2)/Batchsize_test
+    x1 = torch.from_numpy(x)
+    x11 = x1.reshape(-1,28*28)
+    logp2 = f.logProbability(x11)
+    loss2= - logp2.mean
     print('After Training.\nTest loss = %.4f' %loss2)
 
 
