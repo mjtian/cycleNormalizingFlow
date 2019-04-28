546da1c3dc1baf32b910257b505bf2c1763ac368
Li SH <li012589li@outlook.com>
2019-04-29 01:20:00 +0800

    update

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def train():
     lr = 1e-3
     Epoch = 10
     Batchsize_test = 20
-    Batchsize_train = 60000
+    Batchsize_train = 600
     Iteration = len(train_data) // Batchsize_train
     # an epoch means running through the training set roughly once
 
@@ -39,7 +39,10 @@ def train():
     print ('total nubmer of trainable parameters:', nparams)
     optimizer = torch.optim.Adam(params, lr=lr)
 
+    TrainLOSS = []
+    TestLOSS = []
     for epoch in range(Epoch):
+
         for j in range(Iteration):
            x_train= random_draw(train_data,Batchsize_train)
            x_train1 = torch.from_numpy(x_train).to(torch.float32)
@@ -47,11 +50,24 @@ def train():
            logp = f.logProbability(x_train11)
            loss = -logp.mean()
 
+           TrainLOSS.append(loss.item())
+
            f.zero_grad()
            loss.backward()
            optimizer.step()
 
+           x = random_draw(test_data, Batchsize_test)
+           x1 = torch.from_numpy(x).to(torch.float32)
+           x11 = x1.reshape(-1,28*28)
+           logp2 = f.logProbability(x11)
+           loss2= -logp2.mean()
+
+           TestLOSS.append(loss2.item())
+
         print("epoch = %d, loss = %.4f" %(epoch, loss))
+
+    trainLoss = np.array(TrainLOSS)
+    testLoss = np.array(TestLOSS)
 
     x = random_draw(test_data, Batchsize_test)
     x1 = torch.from_numpy(x).to(torch.float32)
@@ -60,7 +76,16 @@ def train():
     loss2= -logp2.mean()
     print('After Training.\nTest loss = %.4f' %loss2)
 
+    from matplotlib import pyplot as plt
 
+    samples = f.sample(1)[0].detach().numpy().reshape(28,28)
+    plt.matshow(samples)
+    plt.figure()
+    plt.plot(trainLoss,label="Training")
+    plt.plot(testLoss,label="Test")
+    plt.legend()
+
+    plt.show()
 
 if __name__ == "__main__":
     train()
