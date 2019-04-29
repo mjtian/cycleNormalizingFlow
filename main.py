39c6dd9b5274dea7a66f525696d3ab6d99d58e22
Li SH <li012589li@outlook.com>
2019-04-30 00:49:49 +0800

    debug

--- main.py
+++ main.py
@@ -25,8 +25,8 @@ def train():
     tList = [SimpleMLP([392,392*2,392,392*2,392],[nn.ELU(),nn.ELU(),nn.ELU(),nn.Tanh()]) for _ in range(depth)]
     sList = [SimpleMLP([392,392*2,392,392*2,392],[nn.ELU(),nn.ELU(),nn.ELU(),ScalableTanh(392)]) for _ in range(depth)]
 
-    '''
     maskList = []
+    '''
     b = torch.zeros(1,28*28).byte()
     b[:,:28*28//2] = 1
     for i in range(len(tList)):
